66c4912c3be07b2a3e2388d84ad37e590a6a37c1
lienze <lienze87@gmail.com>
2022-06-23 13:42:52 +0800

    修改页面布局和样式

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <p>React+Vite+ThreeJS</p>
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        count is: {count}
-      </button>
-    </div>
+    <>
+      <div className="header">
+        <p>React+Vite+ThreeJS</p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </div>
+      <div id="content">
+        <div className="left-sidebar">left-bar</div>
+        <div id="myCanvas" className="main-canvas"></div>
+        <div className="right-sidebar">right-bar</div>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -10,8 +10,13 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+
+const domContainer = document.getElementById("myCanvas");
+if (domContainer) {
+  let box = domContainer.getBoundingClientRect();
+  renderer.setSize(box.width, box.height);
+  domContainer.appendChild(renderer.domElement);
+}
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
